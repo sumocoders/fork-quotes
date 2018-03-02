d237047e01417ced4087ea997b3fe7c89e097abf
StijnVrolijk <stijn.o.vrolijk@gmail.com>
2018-03-02 11:22:25 +0100

    Send the locale as a parameter from the frontend widgets

--- src/Frontend/Modules/Quotes/Widgets/AllQuotes.php
+++ src/Frontend/Modules/Quotes/Widgets/AllQuotes.php
@@ -3,6 +3,7 @@
 namespace Frontend\Modules\Quotes\Widgets;
 
 use Frontend\Core\Engine\Base\Widget as FrontendBaseWidget;
+use Frontend\Core\Language\Locale;
 
 final class AllQuotes extends FrontendBaseWidget
 {
@@ -15,7 +16,7 @@ final class AllQuotes extends FrontendBaseWidget
 
     private function parse()
     {
-        $quotes = $this->get('quotes_repository')->getAllQuotes();
+        $quotes = $this->get('quotes_repository')->getAllQuotes(Locale::frontendLanguage());
 
         $this->template->assign('quotes', $quotes);
     }
--- src/Frontend/Modules/Quotes/Widgets/RandomQuote.php
+++ src/Frontend/Modules/Quotes/Widgets/RandomQuote.php
@@ -4,6 +4,7 @@ namespace Frontend\Modules\Quotes\Widgets;
 
 use Backend\Modules\Quotes\Entity\Quote;
 use Frontend\Core\Engine\Base\Widget as FrontendBaseWidget;
+use Frontend\Core\Language\Locale;
 
 final class RandomQuote extends FrontendBaseWidget
 {
@@ -16,7 +17,7 @@ final class RandomQuote extends FrontendBaseWidget
 
     private function parse()
     {
-        $quote = $this->get('quotes_repository')->getRandomQuote();
+        $quote = $this->get('quotes_repository')->getRandomQuote(Locale::frontendLanguage());
 
         if ($quote instanceof Quote) {
             $this->template->assign('quote', $quote);
